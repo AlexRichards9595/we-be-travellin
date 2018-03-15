ade084db00ddbb13a774398a75d0ebb79d0fedf9
Alex <wcadidas15@gmail.com>
2018-03-15 10:12:01 -0400

    Initial commit

---
+++ src/main/java/org/wecancodeit/columbus/webetravelin/WeBeTravelinApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.columbus.webetravelin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeBeTravelinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeBeTravelinApplication.class, args);
+	}
+}
